166a8cf830dde8ec22d8563ac8c67a176e0ae702
vins-stha <shrs_samar@yahoo.com>
2022-08-04 22:55:27 +0300

    navbar gui v1

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+
+function index() {
+  return (
+    <>
+    <div></div>
+    <Navbar/>
+    <Sidebar/>
+    </>
+  )
+}
+
+export default index
